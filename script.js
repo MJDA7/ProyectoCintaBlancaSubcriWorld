8e16ece97ff95104a9b4a4997485255cd9f55060
anavera31 <anyrrc31@gmail.com>
2020-05-20 01:41:52 -0500

    Enlaces a pagina principal y página de login

---
+++ script.js
@@ -0,0 +1,27 @@
+function nosotros(){
+    window.scrollTo({
+        top: 600,
+        behavior: 'smooth'
+    });
+}
+
+function productos(){
+    window.scrollTo({
+        top: 1150,
+        behavior: 'smooth'
+    });
+}
+
+function planes(){
+    window.scrollTo({
+        top: 2650,
+        behavior: 'smooth'
+    });
+}
+
+function subir(){
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
